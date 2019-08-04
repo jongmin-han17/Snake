9e08211bec47bc5d40a00e88964d49f92e9c0d64
Jongmin Han <33759102+jongminHan@users.noreply.github.com>
2019-08-04 17:40:05 +0900

    Connect Joystick input and menu options

--- SnakeSFML/GameMenuState.cpp
+++ SnakeSFML/GameMenuState.cpp
@@ -2,6 +2,7 @@
 #include "Game.h"
 
 GameMenuState::GameMenuState()
+	: mButtonFocus(START)
 {
 	// Initialize Start Button.
 	if (!mStartButtonTexture.loadFromFile("resources/startButton.png"))
@@ -10,14 +11,36 @@ GameMenuState::GameMenuState()
 		return;
 	}
 
-	mStartButton.setTexture(mStartButtonTexture);
-	mStartButton.setPosition(565, 380);
-
 	if (!mQuitButtonTexture.loadFromFile("resources/quitButton.png"))
 	{
 		std::cout << "Cannot load image.\n";
 		return;
 	}
+
+
+	if (!mGameTitleTexture.loadFromFile("resources/gameTitle.png"))
+	{
+		std::cout << "Cannot load image.\n";
+		return;
+	}
+	
+	if (!mStartButtonFocusTexture.loadFromFile("resources/startButtonFocus.png"))
+	{
+		std::cout << "Cannot load image.\n";
+		return;
+	}
+
+	if (!mQuitButtonFocusTexture.loadFromFile("resources/quitButtonFocus.png"))
+	{
+		std::cout << "Cannot load image.\n";
+		return;
+	}
+
+	mGameTitle.setTexture(mGameTitleTexture);
+	mGameTitle.setPosition(450, 200);
+
+	mStartButton.setTexture(mStartButtonFocusTexture);
+	mStartButton.setPosition(565, 380);
 
 	mQuitButton.setTexture(mQuitButtonTexture);
 	mQuitButton.setPosition(565, 440);
@@ -25,9 +48,34 @@ GameMenuState::GameMenuState()
 
 void GameMenuState::Run(Game& game)
 {
+
+	if (sf::Joystick::isConnected(0))
+	{
+		float y = sf::Joystick::getAxisPosition(0, sf::Joystick::Y);
+
+		if (y > 20.f) // Set dead zone
+		{
+			if (mButtonFocus == START)
+			{
+				mButtonFocus = QUIT;
+				mStartButton.setTexture(mStartButtonTexture);
+				mQuitButton.setTexture(mQuitButtonFocusTexture);
+			}
+		}
+
+		if (y < -20.f) // Set dead zone
+		{
+			if (mButtonFocus == QUIT)
+			{
+				mButtonFocus = START;
+				mStartButton.setTexture(mStartButtonFocusTexture);
+				mQuitButton.setTexture(mQuitButtonTexture);
+			}
+		}
+	}
+
 	game.GetWindow().clear(sf::Color(0, 0, 0));
-	// Draw the pause message
-	game.GetWindow().draw(game.GetPauseMessage());
+	game.GetWindow().draw(mGameTitle);
 	game.GetWindow().draw(mStartButton);
 	game.GetWindow().draw(mQuitButton);
 	game.GetWindow().display();
--- SnakeSFML/GameMenuState.h
+++ SnakeSFML/GameMenuState.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "IGameState.h"
+#include "eButtonFocus.h"
 
 class GameMenuState : public IGameState
 {
@@ -8,8 +9,13 @@ public:
 	virtual ~GameMenuState() = default;
 	void Run(Game& game);
 private:
+	sf::Sprite mGameTitle;
 	sf::Sprite mStartButton;
 	sf::Sprite mQuitButton;
 	sf::Texture mStartButtonTexture;
 	sf::Texture mQuitButtonTexture;
+	sf::Texture mGameTitleTexture;
+	sf::Texture mQuitButtonFocusTexture;
+	sf::Texture mStartButtonFocusTexture;
+	eButtonFocus mButtonFocus;
 };
